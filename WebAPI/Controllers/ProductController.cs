16854c6fc0723588bb1341b43e9ff622abcac7fc
Musab Akan <musab.akan@yenibiris.com>
2023-06-04 23:39:39 +0300

    #0406232339

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetList()
+        {
+            var result = _productService.GetList();
+            return result.Success ? Ok(result.Data) : BadRequest(result.Message);
+        }
+        [HttpGet("getlistbycategory")]
+        public IActionResult GetListByCategory(int categoryId)
+        {
+            var result = _productService.GetListByCategory(categoryId);
+            return result.Success ? Ok(result.Data) : BadRequest(result.Message);
+        }
+        [HttpGet("get")]
+        public IActionResult GetById(int productId)
+        {
+            var result = _productService.GetById(productId);
+            return result.Success ? Ok(result.Data) : BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            return result.Success ? Ok(result) : BadRequest(result.Message);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Product product)
+        {
+            var result = _productService.Update(product);
+            return result.Success ? Ok(result) : BadRequest(result.Message);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Product product)
+        {
+            var result = _productService.Delete(product);
+            return result.Success ? Ok(result) : BadRequest(result.Message);
+        }
+
+    }
+}
